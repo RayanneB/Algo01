13a9cd6133c6289e17a195a610838af5be935890
NathanDouillet <25718810+NathanDouillet@users.noreply.github.com>
2020-11-07 16:11:38 +0100

    fix greedy

--- greedy_snowplow.py
+++ greedy_snowplow.py
@@ -1,30 +1,39 @@
-import math
 import numpy as np
+
+
+def find_nearest(current_node, positions):
+    nearest = None
+    smallest_distance = None
+    for position in positions:
+        if (position == None or current_node != position):
+            distance = abs(position - current_node)
+            if smallest_distance == None or distance < smallest_distance:
+                nearest = position
+                smallest_distance = distance
+    return nearest, smallest_distance
+
 
 def parcours(positions):
     sorted = list()
-    tmp = list(positions)
-    total_distance = 0
-    current_position = 0
+    tmp = list([0] + positions)
+    current_node = 0
+    distance = 0
+    while tmp:
+        nearest_node, nearest_distance = find_nearest(current_node, tmp)
+        if nearest_node == None:
+            break
+        else:
+            distance += nearest_distance
+            sorted.append(nearest_node)
+            tmp.pop(tmp.index(current_node))
+            current_node = nearest_node
+    return sorted, distance
 
-    while(len(tmp) > 1):
-        current_distance = None
-        nearest_position = 0
-        for j in range(len(tmp)):
-            if j != current_position:
-                distance = abs(tmp[j] - tmp[current_position])
-                if current_distance == None or current_distance > distance:
-                    current_distance = distance
-                    nearest_position = tmp[j]
-
-        sorted.append(tmp[current_position])
-        tmp.pop(current_position)
-        current_position = tmp.index(nearest_position)
-        total_distance += current_distance
-
-    return sorted, total_distance + abs(positions[0] - tmp[0])
 
 if __name__ == '__main__':
-    nbrs = np.random.normal(0, 1000, 1000).tolist()
-    sorted, total = parcours(nbrs)
-    print(sorted, total)
+    houses = np.random.normal(0, 5, 5).tolist()
+    sorted, distance = parcours(houses)
+
+    for nb in sorted:
+        print(f"({nb})", end=" --> ")
+    print("Travel time :", distance)
