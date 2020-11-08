51014329576d7bee58319e39f28f4b2471e3de66
Rayanne Bouslimi <rayane.bouslimi@epitech.eu>
2020-11-08 16:42:55 +0100

    merge both greedy algorythm, start implemting our algorythm

--- greedy_snowplow.py
+++ greedy_snowplow.py
@@ -1,6 +1,39 @@
 import numpy as np
 
 
+
+def find_closest(current_position, positions):
+    """
+        Find the closest position from param:current_position,
+        and return it with it's index
+    """
+    if not positions:
+        return None
+    positions = np.asarray(positions)
+    idx = (np.abs(positions - current_position)).argmin()
+    return positions[idx]
+
+def greedy_parcours(positions):
+    """ returns a list that route the snowplow 
+        to the closest house each time
+        greedy algorythm
+    """
+    visited_nodes = list()
+    current_position = 0
+    distance = 0
+    while positions:
+        if current_position != 0:
+            visited_nodes.append(current_position)
+            positions.remove(current_position)
+        next_node = find_closest(current_position, positions)
+        if next_node is None:
+            break
+        distance = distance + abs(next_node - current_position)
+        current_position = next_node
+
+    return visited_nodes, distance
+
+
 def find_nearest(current_node, positions):
     nearest = None
     smallest_distance = None
@@ -31,9 +64,11 @@ def parcours(positions):
 
 
 if __name__ == '__main__':
-    houses = np.random.normal(0, 5, 5).tolist()
+    houses = np.random.normal(0, 1000, 1000).tolist()
     sorted, distance = parcours(houses)
+    sorted_2, distance_2 = greedy_parcours(houses)
 
     for nb in sorted:
         print(f"({nb})", end=" --> ")
     print("Travel time :", distance)
+    print(f"Distance to beat : ", (distance / 10) * 9)
--- snowplow.py
+++ snowplow.py
@@ -1,10 +1,59 @@
-def our_parcours(positions):
-    """ returns a list that route the snowplow 
-        to optimize waiting time
-    """
-    visited_nodes = list()
-    current_position = 0
+import numpy as np
+import sys
+from pprint import pprint
+
+TOTAL_HOUSE_NB = 100
+NB_OF_ZONES = 10
+HOUSE_PER_ZONE = TOTAL_HOUSE_NB / NB_OF_ZONES
+
+np.set_printoptions(suppress=True, threshold=sys.maxsize)
+
+class Solver(): 
+
+    def __init__(self, original_list):
+        self.original_list = original_list
+        self.sorted_matrice = None
+        self.minimum_spanning_tree = list()
+    
+    def chunks(self, lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
+    def sort_and_cluster_list(self):
+            self.sorted_matrice = sorted(self.original_list) # Sort positions from - to +
+            self.sorted_matrice = list(self.chunks(self.sorted_matrice, int(HOUSE_PER_ZONE)))
+            
+    def convert_and_sort_positions_to_edges(self, zone):
+        edges_list = list()
+        for index, node in enumerate(zone):
+            
+            if index != HOUSE_PER_ZONE - 1:
+                edges_list.append({
+                    'vertices': [zone[index], zone[index + 1]],
+                    'weight': abs(zone[index] - zone[index + 1])
+                })
+
+        edges_list = sorted(edges_list, key = lambda elem: elem['weight'])
+
+        return edges_list
 
 
+    def solve(self):
+        """ returns a list that route the snowplow 
+            to optimize waiting time
+        """
+        self.sort_and_cluster_list()
+
+        visited_nodes = list()
+        current_position = 0
+        for index, zone in enumerate(self.sorted_matrice):
+            # convert positions to nodes
+            self.sorted_matrice[index] = self.convert_and_sort_positions_to_edges(zone)
+            pprint(self.sorted_matrice[index])
+            
+
+        
 if __name__ == '__main__':
-    our_parcours(np.random.normal(0, 1000, 1000).tolist())
+    my_solver = Solver(np.random.normal(0, TOTAL_HOUSE_NB, TOTAL_HOUSE_NB).tolist())
+    my_solver.solve()
